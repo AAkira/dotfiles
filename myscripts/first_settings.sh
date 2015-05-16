43ab96ceccab71b91e5b973bd6fe90c7651a68d9
A.Akira <developer.a.akira@gmail.com>
2015-05-16 15:16:38 +0900

    add node install scripts

--- myscripts/first_settings.sh
+++ myscripts/first_settings.sh
@@ -3,5 +3,14 @@
 # ctrl + sで履歴を進める機能を有効にする
 stty stop undef
 
+# homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # neobundle install
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+# nodebrew
+brew install nodebrew
+# nodejs
+sudo nodebrew install-binary stable
+sudo nodebrew use stable
