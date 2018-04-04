43917d5cd23ecc51ccf9b4392ab01f9b33861fb7
A.Akira <developer.a.akira@gmail.com>
2018-04-04 21:38:18 +0900

    Update key note highlight.

--- myscripts/keynote-highlight.sh
+++ myscripts/keynote-highlight.sh
@@ -1,1 +1,2 @@
-pbpaste | highlight -O rtf -S Kotlin -s Seashell -K 32 -k 'Source Code Pro' | pbcopy
+#pbpaste | highlight -O rtf -S Kotlin -s Seashell -K 32 -k 'Source Code Pro' | pbcopy
+pbpaste | highlight -O rtf -S Kotlin -s Moria -K 32 -k 'Source Code Pro' | pbcopy
