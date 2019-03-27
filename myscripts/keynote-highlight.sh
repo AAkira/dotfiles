7276f2ab367204473f3e3becf59f693c13d71f4f
AAkira <developer.a.akira@gmail.com>
2019-03-27 12:41:06 +0900

    Update keynote highlight.

--- myscripts/keynote-highlight.sh
+++ myscripts/keynote-highlight.sh
@@ -1,3 +1,3 @@
 #pbpaste | highlight -O rtf -S Kotlin -s Seashell -K 32 -k 'Source Code Pro' | pbcopy
-#pbpaste | highlight -O rtf -S Gradle -s Moria -K 68 -k 'Source Code Pro' | pbcopy
-pbpaste | highlight -O rtf -S Kotlin -s Moria -K 48 -k 'Source Code Pro' | pbcopy
+pbpaste | highlight -O rtf -S Groovy -s Moria -K 38 -k 'Source Code Pro' | pbcopy
+#pbpaste | highlight -O rtf -S Kotlin -s Moria -K 48 -k 'Source Code Pro' | pbcopy
