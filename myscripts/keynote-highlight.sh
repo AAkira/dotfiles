83d14bb32847c1288804bf893841b97792204e03
AAkira <developer.a.akira@gmail.com>
2023-11-09 17:19:10 +0900

    Update mis scripts.

--- myscripts/keynote-highlight.sh
+++ myscripts/keynote-highlight.sh
@@ -1,4 +1,7 @@
 #pbpaste | highlight -O rtf -S Kotlin -s Seashell -K 32 -k 'Source Code Pro' | pbcopy
 #pbpaste | highlight -O rtf -S Groovy -s Moria -K 38 -k 'Source Code Pro' | pbcopy
-pbpaste | highlight -O rtf -S Kotlin -s Moria -K 48 -k 'Source Code Pro' | pbcopy
+#pbpaste | highlight -O rtf -S Kotlin -s Moria -K 48 -k 'Source Code Pro' | pbcopy
 #pbpaste | highlight -O rtf -S xml -s Moria -K 48 -k 'Source Code Pro' | pbcopy
+
+# Flutter Kaigi 2023
+pbpaste | highlight -O rtf -S Dart -s Seashell -K 32 -k 'Source Code Pro' | pbcopy
